db7f2fa4522897d135c5e5147c4d4e9a3a1dd19d
Alessandro Cambiaghi <a.cambiaghi@codeway.ch>
2025-01-22 15:05:07 +0100

    test deploy gh

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build"
-  },
-  server: {
-    host:"0.0.0.0",
-    port:3500,
-    strictPort: true,
-    hmr: {
-      clientPort: 443 // Run the websocket server on the SSL port
-    }
-  }
+  base:"/macbet_react"
+  // build: {
+  //   outDir: "build"
+  // },
+  // server: {
+  //   host:"0.0.0.0",
+  //   port:3500,
+  //   strictPort: true,
+  //   hmr: {
+  //     clientPort: 443 // Run the websocket server on the SSL port
+  //   }
+  // }
 });
