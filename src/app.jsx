206be9acbe1a53f704368495c580a893be01a66c
Alessandro Cambiaghi <a.cambiaghi@codeway.ch>
2025-06-09 14:53:51 +0200

    try zero start

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Benvenuto su React + Vite!</h1>
+      <p>Questo è il tuo progetto base pronto per deploy su Render.</p>
+    </div>
+  )
+}
